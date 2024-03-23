f3f25162eac58387472ad9fcc40ed387783bae93
beniroquai <bene.d@gmx.de>
2024-03-23 18:51:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+with open("HISTORY.rst", "r") as fh:
+    history = fh.read()
 
 requirements = [ ]
 
@@ -32,6 +32,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
+    long_description_content_type="text/x-rst",    
     include_package_data=True,
     keywords='imswitchclient',
     name='imswitchclient',
