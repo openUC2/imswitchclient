dd3d5c67447388503577d989d06cdf93a038748d
beniroquai <bene.d@gmx.de>
2024-04-17 23:04:30 +0200

    adding speed to the positioner

--- imswitchclient/positionersManager.py
+++ imswitchclient/positionersManager.py
@@ -11,14 +11,34 @@ class positionersManager(object):
         response = self.parent.get_json(url, headers=headers)
         return response
     
-    def movePositioner(self, positioner_name, axis, dist, is_absolute=True, is_blocking=True):
+    def movePositionerForever(self, speed=(0, 0, 0, 0), is_stop=False):
+        url = f"{self.parent.base_uri}/PositionerController/movePositionerForever"
+        payload = {
+            'speed': speed,
+            'isStop': is_stop
+        }
+        response = self.parent.get_json(url, payload=payload)
+        return response       
+            
+    def setPositionerSpeed(self, positioner_name, axis, speed):
+        url = f"{self.parent.base_uri}/PositionerController/setPositionerSpeed"
+        payload = {
+            'positionerName': positioner_name,
+            'axis': axis,
+            'speed': speed
+        }
+        response = self.parent.get_json(url, payload=payload)
+        return response
+        
+    def movePositioner(self, positioner_name, axis, dist, is_absolute=True, is_blocking=True, speed=10000):
         url = f"{self.parent.base_uri}/PositionerController/movePositioner"
         payload = {
             'positionerName': positioner_name,
             'axis': axis,
             'dist': dist,
             'isAbsolute': is_absolute,
-            'isBlocking': is_blocking
+            'isBlocking': is_blocking, 
+            'speed': speed
         }
         #headers = {'accept': 'application/json'}
 
